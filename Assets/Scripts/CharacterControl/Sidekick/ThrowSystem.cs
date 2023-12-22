e06f8c8fc5ad2a7731f887c6fea1e805e7830c8a
RENNES\o.leclerc <orion.leclerc@gmail.com>
2023-12-22 15:50:50 +0100

    throw sidekick without target

--- Assets/Scripts/CharacterControl/Sidekick/ThrowSystem.cs
+++ Assets/Scripts/CharacterControl/Sidekick/ThrowSystem.cs
@@ -7,19 +7,32 @@ public class ThrowSystem : MonoBehaviour
 {
     [SerializeField] private TargetSystem _targetSystem;
     [SerializeField] private ParticleSystem _correctTrajectoryEmission;
+    [SerializeField] private ParticleSystem _untargetedTrajectoryEmission;
     [SerializeField] private ParticleSystem _retrieveTrajectoryEmission;
     [SerializeField] private Transform _sidekickThrowOrigin;
     [SerializeField] private InputReader _input;
     [SerializeField] private GameObject _sidekickGameObject;
 
     private Target _lockedTarget, _lastActivatedTarget;
+    private Transform _retrievePos;
     private bool _sidekickIsAvailable = true, _throwWasPressedLastFrame, _isRetrieving;
     
+    public Transform RetrievePos { get { return _retrievePos; } set { _retrievePos = value; } }
+
+    private void Awake()
+    {
+        _retrievePos = new GameObject().transform;
+    }
+
     private void Update()
     {
         if (_input.ThrowIsPressed && _sidekickIsAvailable && _targetSystem.currentTarget != null && !_throwWasPressedLastFrame)
         {
-            ThrowSidekick();
+            ThrowSidekickOnTarget();
+        }
+        else if (_input.ThrowIsPressed && _sidekickIsAvailable && _targetSystem.currentTarget == null && !_throwWasPressedLastFrame)
+        {
+            ThrowSidekickUntargeted();
         }
     }
 
@@ -28,11 +41,11 @@ public class ThrowSystem : MonoBehaviour
         _throwWasPressedLastFrame = _input.ThrowIsPressed;
     }
 
-    private void ThrowSidekick()
+    private void ThrowSidekickOnTarget()
     {
         _sidekickGameObject.SetActive(false);
         _lockedTarget = _targetSystem.currentTarget;
-        _lastActivatedTarget = _lockedTarget;
+        _retrievePos = _lockedTarget.transform;
         _correctTrajectoryEmission.transform.position = _lockedTarget.transform.position;
         var shape = _correctTrajectoryEmission.shape;
         shape.position = _correctTrajectoryEmission.transform.InverseTransformPoint(_sidekickThrowOrigin.position);
@@ -40,14 +53,19 @@ public class ThrowSystem : MonoBehaviour
         _sidekickIsAvailable = false;
     }
 
+    private void ThrowSidekickUntargeted()
+    {
+        _untargetedTrajectoryEmission.Play();
+    }
+
     public void RetrieveSidekick()
     {
-        if (_lastActivatedTarget != null)
+        if (_retrievePos != null)
         {
             _isRetrieving = true;
             StartCoroutine(UpdateSidekickRetrievePoint());
             var shape = _retrieveTrajectoryEmission.shape;
-            shape.position = _retrieveTrajectoryEmission.transform.InverseTransformPoint(_lastActivatedTarget.transform.position);
+            shape.position = _retrieveTrajectoryEmission.transform.InverseTransformPoint(_retrievePos.position);
             _retrieveTrajectoryEmission.Play();
         }
     }
