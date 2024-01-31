8b230c982a70f3f87d8b70f8b9f9650a066b556d
Orion Leclerc <orion.leclerc@gmail.com>
2024-01-31 18:41:26 +0100

    miasma disabled in interactor radius + opening / closing notebook

--- Assets/Scripts/EnvironmentInteraction/Miasma/Miasma.cs
+++ Assets/Scripts/EnvironmentInteraction/Miasma/Miasma.cs
@@ -6,28 +6,56 @@ using UnityEngine;
 public class Miasma : MonoBehaviour
 {
     private PlayerController _player;
+    private InteractorScript[] _interactors;
+    private bool _wasInInteractorRadiusLastFrame;
 
     private void Awake()
     {
         _player = GetComponentInParent<PlayerController>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (!_player.IsInMiasma)
+        FindInteractors();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var interactor in _interactors)
         {
-            _player.MiasmaTimer.Start();
+            float interactorRadius = interactor.radius; 
+            Vector3 interactorPosition = interactor.transform.position;
+            Vector3 objectPosition = transform.position;
+
+
+            bool isPointInRadiusRange = Mathf.Pow((objectPosition.x - interactorPosition.x), 2) + Mathf.Pow((objectPosition.y - interactorPosition.y), 2) + Mathf.Pow((objectPosition.z - interactorPosition.z), 2) < Mathf.Pow(interactorRadius, 2);
+
+            if (isPointInRadiusRange)   
+            {
+                ExitMiasma();
+            }
+            _wasInInteractorRadiusLastFrame = isPointInRadiusRange;
+        
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        _player.IsInMiasma = true;
+        if (!_player.IsInMiasma && !_wasInInteractorRadiusLastFrame)
+        {
+            _player.MiasmaTimer.Start();
+            _player.IsInMiasma = true;
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-            _player.IsInMiasma = false;
-            Invoke(nameof(StopMiasmaTimer), 0.2f);
+          ExitMiasma();  
+    }
+
+    private void ExitMiasma()
+    {
+        _player.IsInMiasma = false;
+        Invoke(nameof(StopMiasmaTimer), 0.2f);
     }
     
     private void StopMiasmaTimer()
@@ -36,6 +64,11 @@ public class Miasma : MonoBehaviour
         {
             _player.MiasmaTimer.Stop();
         }
+    }
+    
+    private void FindInteractors()
+    {
+        _interactors = FindObjectsOfType<InteractorScript>();
     }
 
 }
