fbfbdaf1470333bfe29c6a55a28535eb3f4d79ce
RENNES\o.leclerc <orion.leclerc@gmail.com>
2024-02-23 14:19:39 +0100

    all postcards achievement setup

--- Assets/Scripts/UI/Chromachievements/AchievementsCue.cs
+++ Assets/Scripts/UI/Chromachievements/AchievementsCue.cs
@@ -1,32 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AchievementsCue : MonoBehaviour
 {
     [SerializeField] private ChromachievemEventsPublisher _chromachievemEvents;
+    [SerializeField] private TMP_Text _title;
+    [SerializeField] private TMP_Text _description;
+    [SerializeField] private Image _icon;
 
-    private Animator _cueAnimator;
-    private Image _backgroundImage;
+    private List<MaskableGraphic> _uiElements;
+    private GameObject _container;
+    
 
     private void Awake()
     {
-        _cueAnimator = GetComponentInChildren<Animator>();
-        _backgroundImage = GetComponentInChildren<Image>();
+        _chromachievemEvents.ChromachievementUnlocked.AddListener(SendCue);
+
+        _container = transform.GetChild(0).gameObject;
+        
+        _uiElements = new List<MaskableGraphic>();
+
+        foreach (Transform child in GetComponentsInChildren<Transform>())
+        {
+            var element = child.GetComponent<MaskableGraphic>();
+            if(element != null) _uiElements.Add(element);
+        }
+        
+        foreach (var element in _uiElements)
+        {
+            element.canvasRenderer.SetAlpha(0f);
+        }
     }
 
-    private void Start()
+    private void SendCue(Chromachievement chromachievement)
     {
+        _title.text = chromachievement.Name;
+        _description.text = chromachievement.DescriptionText;
+        _icon.sprite = chromachievement.Icon;
         StartCoroutine(Fade());
     }
+
     
     private IEnumerator Fade()
     {
-        _backgroundImage.canvasRenderer.SetAlpha(0);
-        _backgroundImage.CrossFadeAlpha(1f, 5f, true);
-        yield break;
+        foreach (var element in _uiElements)
+        {
+            element.CrossFadeAlpha(1f, 2f, true);
+        }
+        
+        yield return new WaitForSecondsRealtime(5f);
+        
+        foreach (var element in _uiElements)
+        {
+            element.CrossFadeAlpha(0f, 1f, true);
+        }
     }
     
 }
--- Assets/Scripts/UI/Chromachievements/Chromachievement.cs
+++ Assets/Scripts/UI/Chromachievements/Chromachievement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Chromachievement : ScriptableObject
 {
     public Sprite Icon;
-    [TextArea(3,10)] public string Text;
-    public int StickerIndex;
+    public string Name;
+    [TextArea(3,10)] public string DescriptionText;
+    public int ID;
 }
